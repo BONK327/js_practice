423c6e864592c19f0bfa4048c6f1ac2d8d55852a
Vlad Pogrebnoy <myemail@mail.ru>
2024-12-26 15:28:20 +0300

    Practice №4

--- js/main.js
+++ js/main.js
@@ -1,102 +1,72 @@
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?')
-
-    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?')
-    }
-}
-// start();
-
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-}
+    privat: false,
 
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-writeYourGenres();
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat);
-
-function countOfFilms() {
-    if (personalMovieDB.count < 10) {
-        console.log('Просмотренно довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('Вы классический зритель');
-    } else if (personalMovieDB.count >= 30) {
-        console.log('Вы киноман');
-    } else {
-        console.log('Надо вводить положительными числами ;)')
-    }
-}
-// countOfFilms();
-
-
-
-// function rememberMyFilmsFor() {
-//     for (let i = 0; i < 2; i++) {
-//         const a = prompt('Один из последних просмотренных фильмов?'),
-//             b = prompt('На сколько вы бы оценили его?');
-//
-//         if (a != null && b != null && a.length < 50 && a != '' && b != '') {
-//             personalMovieDB.movies[a] = b;
-//             console.log('done');
-//         } else {
-//             console.log('error');
-//             i--;
-//         }
-//     }
-// }
-// rememberMyFilmsFor();
-
-
-// function rememberMyFilmsWhile() {
-//     let i = 0;
-//     while (i < 2) {
-//         const a = prompt('Один из последних просмотренных фильмов?'),
-//             b = prompt('На сколько вы бы оценили его?');
-//
-//         if (a != null && b != null && a.length < 50 && a != '' && b != '') {
-//             personalMovieDB.movies[a] = b;
-//             i++;
-//             console.log('done');
-//         } else {
-//             console.log('error');
-//             i--;
-//         }
-//     }
-// }
-// rememberMyFilmsWhile();
-
-
-function rememberMyFilmsDoWhile() {
-    let i = 0;
-    do {
-        const a = prompt('Один из последних просмотренных фильмов?'),
-            b = prompt('На сколько вы бы оценили его?');
-
-        if (a != null && b != null && a.length < 50 && a != '' && b != '') {
-            personalMovieDB.movies[a] = b;
-            i++;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
+    start: function() {
+        personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?')
+        while(personalMovieDB.count === '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?')
         }
-    } while (i < 2)
+    },
+
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat == false) {
+            personalMovieDB.privat = true;
+        } else {
+            personalMovieDB.privat = false;
+        }
+    },
+
+    rememberMyFilmsFor: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?'),
+                b = prompt('На сколько вы бы оценили его?');
+
+            if (a != null && b != null && a.length < 50 && a != '' && b != '') {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+
+    countOfFilms: function() {
+        if (personalMovieDB.count < 10) {
+            console.log('Просмотренно довольно мало фильмов');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('Вы классический зритель');
+        } else if (personalMovieDB.count >= 30) {
+            console.log('Вы киноман');
+        } else {
+            console.log('Надо вводить положительными числами ;)')
+        }
+    },
+
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+            if (genre === '' || genre == null) {
+                console.log('Вы нажали на кнопку "Отмена" или не ввели данные');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+        }
+        personalMovieDB.genres.forEach(function (item, i) {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`)
+        });
+    }
 }
-// rememberMyFilmsDoWhile();
--- js/script.js
+++ js/script.js
@@ -1,50 +1,16 @@
-// const options = {
-//     width: 100,
-//     height: 100,
-//     color: {
-//         border: 'black',
-//         bg: 'red'
-//     },
-//     test: function () {
-//         console.log('test function')
-//     }
-// };
-// options.test();
-//
-//
-//
-// // const {border, bg} = options.color;
-// // console.log(border, bg)
-//
-// console.log(Object.keys(options).length)
-//
-// for (let key in options) {
-//     if (typeof(options[key]) === 'object') {
-//         for (let i in options[key]) {
-//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//         }
-//     } else {
-//         console.log(`Свойство ${key} имеет значение ${options[key]}`)
-//     }
+let arr = [1, 2, 4, 45, 99];
+
+
+// arr.push(10);
+// console.log(arr.pop())
+const num = prompt("", "");
+const numbers = num.split('; ')
+// console.log(numbers.join('% '))
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i])
 // }
 
-
-const obj = {
-  closes: true,
-  room: 3,
-  laptop: 'Huawei',
-  run: function (i, j) {
-      console.log(`Пробежать можно ${i} километров за ${j} часов`);
-  },
-    obj2: {
-      color: 'blue',
-        bg: 'yellow'
-    }
-};
-obj.run(58, 3);
-
-console.log(Object.keys(obj).length);
-console.log(obj.obj2);
-
-const {color, bg} = obj.obj2;
-console.log(color, bg);
+// arr.forEach(function (item, i, arr) {
+//   console.log(`${i}: ${item} внутри массива ${arr}`);
+// })

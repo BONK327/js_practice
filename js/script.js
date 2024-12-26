ad1cb4dfdfa63ab8e7b60a81fd36b6e6266b87ef
Vlad Pogrebnoy <myemail@mail.ru>
2024-12-26 11:41:39 +0300

    script object

--- js/script.js
+++ js/script.js
@@ -1,8 +1,50 @@
-let js = 'JavaScript';
-let arr = [1, 51, 'askfjsaf', true];
+// const options = {
+//     width: 100,
+//     height: 100,
+//     color: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     test: function () {
+//         console.log('test function')
+//     }
+// };
+// options.test();
+//
+//
+//
+// // const {border, bg} = options.color;
+// // console.log(border, bg)
+//
+// console.log(Object.keys(options).length)
+//
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`)
+//     }
+// }
 
-console.log(js);
-console.log(js.slice(4, 10));
-console.log(3e9)
 
+const obj = {
+  closes: true,
+  room: 3,
+  laptop: 'Huawei',
+  run: function (i, j) {
+      console.log(`Пробежать можно ${i} километров за ${j} часов`);
+  },
+    obj2: {
+      color: 'blue',
+        bg: 'yellow'
+    }
+};
+obj.run(58, 3);
 
+console.log(Object.keys(obj).length);
+console.log(obj.obj2);
+
+const {color, bg} = obj.obj2;
+console.log(color, bg);
